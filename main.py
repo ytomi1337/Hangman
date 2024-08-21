7792d7e77b74dac12a62fd627d95b7f8f6093615
yTomi1337 <laniocha123@gmail.com>
2024-08-21 14:24:07 +0200

    zmiany

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from openpyxl import  Workbook, load_workbook
+from datetime import datetime
 import random
 import string
+import msvcrt
+import os
+
 excel_file = load_workbook('words.xlsx')
 excel_sheet = excel_file.active
+
+excel_history_file = load_workbook('game_history.xlsx')
+excel_history_sheet = excel_history_file.active
 
 def word_roll():
     words_list = []
@@ -14,75 +21,107 @@ def word_roll():
     return str(random_word).upper()
 
 def hangman_quess():
-
-
-
+    username = input("Podaj nazwe gracza: ")
     word = word_roll()
     word_letters = list(word)
     alphabet = set(string.ascii_uppercase)
     used_letter = set()
     lifes =['<3', '<3', '<3', '<3', '<3', '<3']
+    attemps = 1
+    data = datetime.now()
+    format_date = data.strftime("%d.%m.%Y")
 
     for cell in excel_sheet['A']:
-        if cell.value == str(word):
+        if cell.value == word:
             nr_cell = str(cell.coordinate)
-            nr_cell = nr_cell.lower().replace("a", "B")
+            nr_cell = nr_cell.upper().replace("A", "B")
 
             category = excel_sheet[nr_cell].value
 
 
     while len(word_letters) > 0:
-        print(len(word_letters))
-        print(word_letters)
-
+        
         if lifes == []:
             print("Koniec gry ! Przegrana, słowo: ", word)
+            print("")
+            print("Kliknij dowolny przycisk by wrocic do menu....")
+            msvcrt.getch()
+            os.system('cls')
 
-            while True:
-                print("1.Zagraj Ponownie")
-                print("2.Menu")
-                print("")
-                wybor = input("Wybor: ")
-                print(wybor)
-                if wybor == '1':
-                    hangman_quess()
-                elif wybor == '2':
-                    return
-                else:
-                    print('Nie prawidlowa wartosc')
-
-
+            excel_history_sheet.append([username, format_date, word, len(lifes), attemps, 'Przegrana'])
+            excel_history_file.save('game_history.xlsx')
+            return
 
         else:
+            print("")
             print('Kategoria:',category,'Zycia:',' '.join(lifes))
             print('Zużyte litery: ', ' '.join(used_letter))
             word_blank = [letter if letter in used_letter else '-' for letter in word]
             print('Słowo: ', ' '.join(word_blank))
+            print("")
             answer = input("Zgadnij wyraz: ").upper()
+
+            attemps += 1
 
             if answer in alphabet - used_letter:
                 used_letter.add(answer)
                 if answer in word_letters:
+                    os.system('cls')
                     for i,x in enumerate(word_letters):
                         if x == answer:
                             word_letters.remove(answer)
+             
                 else:
+                    os.system('cls')
                     lifes.remove('<3')
+            
 
             elif answer in used_letter:
+                print("")
                 print(f'Literka: {answer} Została już wprowadzona!')
+                print("")
             else:
                 print('Nie prawidłowa wartość!')
+                attemps -= 1
+    
+    print(f'Gratulacje zgadłeś! Słowo: {word} Liczba prób: {attemps}')
+    print("")
+    print("Kliknij dowolny przycisk by wrocic do menu....")
+    msvcrt.getch()
+    os.system('cls')
+    
 
+    excel_history_sheet.append([username, format_date, word, len(lifes), attemps, 'Wygrana'])
+    excel_history_file.save('game_history.xlsx')
+            
 
+def history():
+    for row in excel_history_sheet.iter_rows(2, excel_history_sheet.max_row, values_only=True):
+        for value in row:
+            print(value, end='  |  ')
+        print()
+    
+    print("")
+    print("Kliknij dowolny przycisk by wrocic do menu....")
+    print("")
+    msvcrt.getch()
+    os.system('cls')
 
+def add_word():
+    new_word = str(input("Podaj słowo: ")).upper()
+    new_category = str(input("Podaj Kategorie: ")).upper()
+    
+    
+    excel_sheet.append([new_word, new_category])
+    excel_file.save('words.xlsx')
 
-
-
-
-
-
-
+    print("")
+    print("Dodano Poyślnie!")
+    print("")
+    print("Kliknij dowolny przycisk by wrocic do menu....")
+    print("")
+    msvcrt.getch()
+    os.system('cls')
 
 def main():
     choice_list = ['1', '2', '3', '4']
@@ -96,13 +135,23 @@ def main():
         choice = input("Wybór: ")
 
         if choice == '1':
+            os.system('cls')
             hangman_quess()
         elif choice == '2':
-            print("Dodaj")
+            os.system('cls')
+            add_word()
         elif choice == '3':
-            print("Historia gier")
+            os.system('cls')
+            print("GRACZ  |  DATA ROZGRYWKI  |  SŁOWO  |  ZYC  |  PROB  |  WYNIK  |")
+            history()
         elif choice == '4':
-            print("opuszczanie")
+            os.system('cls')
+            print("Opuszczanie gry")
+            print("")
+            print("Kliknij dowolny przycisk by wrocic do menu....")
+            print("")
+            msvcrt.getch()
+            return
         elif choice is not choice_list:
             print("Nie prawidłowy wybór ! Wpisz ponownie")
             print("")
